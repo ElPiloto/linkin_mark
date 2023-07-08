945692467f3b97f510585cfae4bedffe32fb860c
ElPiloto <luis.r.piloto@gmail.com>
2023-07-08 23:36:21 +0100

    Add two more tests.

--- test/test_lib.py
+++ test/test_lib.py
@@ -34,3 +34,46 @@ class TestLinks(unittest.TestCase):
         links,
         'Did not find links in markdown files'
     )
+
+  def test_get_relativized_links(self):
+    links = lm.get_relativized_links(self.data_path)
+    index_md = pathlib.Path('index.md')
+    one_md = pathlib.Path('linkedto1.md')
+    two_md = pathlib.Path(os.path.join('subdir', 'linkedto2.md'))
+    expected_links = {
+        index_md: [one_md, two_md],
+        one_md: [],
+        two_md: [one_md],
+    }
+    self.assertSetEqual(
+        set(expected_links.keys()),
+        set(links.keys()),
+        'Did not find expected markdown files',
+    )
+    self.assertDictEqual(
+        expected_links,
+        links,
+        'Did not find links in markdown files'
+    )
+
+  def test_get_links_excluded(self):
+    """Makes sure excluded files are actually excluded."""
+    links = lm.get_links(self.data_path, excluded_files=['index.md'])
+    one_md = pathlib.Path(os.path.join(self.data_path, 'linkedto1.md'))
+    two_md = pathlib.Path(
+        os.path.join(self.data_path, 'subdir', 'linkedto2.md')
+    )
+    expected_links = {
+        one_md: [],
+        two_md: [one_md],
+    }
+    self.assertSetEqual(
+        set(expected_links.keys()),
+        set(links.keys()),
+        'Did not find expected markdown files',
+    )
+    self.assertDictEqual(
+        expected_links,
+        links,
+        'Did not find links in markdown files'
+    )
